ba3c742a2543c02e6e6e4273a1a30c31d04e606b
gpswalika <hackertimoarm@gmail.com>
2021-06-03 16:20:59 +0900

    2021-06-03

--- main.py
+++
@@ -1,9 +0,0 @@
-def solution(maps):
-    answer = 0
-    n_len = len(maps)
-    m_len = len(maps[0])
-
-    if not maps[n_len - 2][m_len - 1] or not maps[n_len - 1][m_len - 2]:
-        return -1
-
-    return answer
